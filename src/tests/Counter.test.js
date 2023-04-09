29fe12ecc242c7cf4efabb73ec87d56dbb9ff400
m-bilal-ahmed <m.bilalahmed2k@gmail.com>
2023-04-09 00:11:17 -0400

    re tested my changes.

--- src/tests/Counter.test.js
+++ src/tests/Counter.test.js
@@ -3,28 +3,28 @@ import Counter from '../components/Counter';
 
 test('renders counter message', () => {
   render(<Counter />);
-  const messageElement = screen.getByText(/count/i);
-  expect(messageElement).toBeInTheDocument();
+  const message_to_send = screen.getByText(/count/i);
+  expect(message_to_send).toBeInTheDocument();
 });
 
 test('should render initial count with value of 0', () => {
   render(<Counter />);
-  const countElement = screen.getByTestId('count');
-  expect(countElement.textContent).toBe('0');
+  const elementCounter = screen.getByTestId('count');
+  expect(elementCounter.textContent).toBe('0');
 });
 
 test('clicking + increments the count', () => {
   render(<Counter />);
   const incrementButton = screen.getByRole('button', { name: '+' });
   fireEvent.click(incrementButton);
-  const countElement = screen.getByTestId('count');
-  expect(countElement.textContent).toBe('1');
+  const elementCounter = screen.getByTestId('count');
+  expect(elementCounter.textContent).toBe('1');
 });
 
 test('clicking - decrements the count', () => {
   render(<Counter />);
   const decrementButton = screen.getByRole('button', { name: '-' });
   fireEvent.click(decrementButton);
-  const countElement = screen.getByTestId('count');
-  expect(countElement.textContent).toBe('-1');
+  const elementCounter = screen.getByTestId('count');
+  expect(elementCounter.textContent).toBe('-1');
 });
